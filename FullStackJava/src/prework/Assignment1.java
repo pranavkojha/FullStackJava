750bc0a2f7a44241e346ac2dfa8b14b374838c72
Pranav Kumar Ojha <pranav.ojha@Pranavs-iMac.local>
2023-09-17 14:28:27 +0530

    Added comments to make code readable

--- FullStackJava/src/prework/Assignment1.java
+++ FullStackJava/src/prework/Assignment1.java
@@ -1,5 +1,5 @@
 /**
- * Author: pranavkojha@gmail.com 
+ * Author: pranavkojha@gmail.com
  */
 package prework;
 import java.util.Scanner;
@@ -7,14 +7,25 @@ import java.util.Scanner;
 public class Assignment1 {
     static Scanner sc = new Scanner(System.in);
     public void checkPalindrome()  {
+        /** This method accepts a number and prints if it is a palindrome or not.
+         *
+         */
+        //Accept User input
         System.out.print("Enter number to check for palindrome:");
         int num = sc.nextInt();
+
+        //Let's convert all negative numbers to positive.
         if(num<0) {
             num *= -1;
         }
+
+        // All 1-digit numbers are palindrome.
         if(num<10) {
             System.out.println("Number " + num + " is a palindrome.");
+            return;
         }
+
+        // Core logic to reverse the number.
         byte count = 0;
         int rev_num=0, tmp = num;
         while(tmp > 0) {
@@ -22,7 +33,8 @@ public class Assignment1 {
             rev_num = rev_num * 10 + reminder;
             tmp /= 10; count++;
         }
-        //System.out.println("num:" + num + ", rev_num:" + rev_num);
+
+        // If reversed number is same as actual number, it was a palindrome.
         if (rev_num == num)
             System.out.println("Number " + num + " is a palindrome.");
         else
@@ -30,8 +42,21 @@ public class Assignment1 {
     }
 
     public void printPattern() {
+        /**
+         * This method print "*" starting with the number entered by user and then prints this
+         * recursively starting with the largest number, reducing the print by 1 char each time
+         * something like this:
+         * *****
+         * ****
+         * ***
+         * **
+         * *
+         */
+        // Accept user input.
         System.out.print("Enter number to print the pattern:");
         int num = sc.nextInt();
+
+        // Start printing * starting with largest number and reducing one count every new line.
         for (int i=num; i>0; i--) {
             for (int j=0; j < i; j++)
                 System.out.print("*");
@@ -40,8 +65,15 @@ public class Assignment1 {
     }
 
     public void checkPrimeNumber() {
+        /**
+         * This method checks if a given number is prime or not.
+         */
+
+        // Accept user input.
         System.out.print("Enter number to check if it is Prime number or not:");
         int num = sc.nextInt();
+
+        // Handle the numbers 1-3.
         if(num == 1) {
             System.out.println("1 is NOT a Prime number");
             return;
@@ -54,6 +86,8 @@ public class Assignment1 {
             System.out.println(num + " is a prime number");
             return;
         }
+
+        // If a number N is divisble by N/2 numbers, it is NOT a prime number.
         for(int i=3; i<num/2; i+=2)
         {
             if(num%i == 0) {
@@ -61,12 +95,20 @@ public class Assignment1 {
                 return;
             }
         }
+
+        // If a number N is not divisble by N/2 numbers, it IS a prime number.
         System.out.println(num + " is a prime number");
     }
 
     public void printFibonacciSeries() {
+        /**
+         * This method prints the first N fibonacci numbers.
+         */
         int num = -1;
+        // Accept user input.
         System.out.print("Enter number to check if it is Prime number or not:");
+
+        // Btw, the first 2 number in a fibonacci series are fixed. Let us ask for a number >=2.
         while(num < 2) {
             num = sc.nextInt();
             if(num < 2)
@@ -74,6 +116,7 @@ public class Assignment1 {
         }
 
         int first = 0, second = 1;
+        // Start printing the fibonacci series
         System.out.print(first + ", " + second + ", ");
         for(int i=2; i<num; i++) {
             int tmp = first;
@@ -83,10 +126,15 @@ public class Assignment1 {
         }
     }
     public static void main(String[] args) {
+        /**
+         * This is the main method that runs an infinite do-while loop till the user choses to
+         * exit.
+         */
         Assignment1 obj = new Assignment1();
         int choice;
         Scanner sc = new Scanner(System.in);
         do {
+            // Accept user input.
             System.out.println("\n================\nChose from below list:\n" +
                     "1. Find palindrome of number.\n" +
                     "2. Print Pattern for a given no.\n" +
